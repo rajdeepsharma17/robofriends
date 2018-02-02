3bb987e372ae370c72449c526e64724ca488105a
rajdeepsharma17 <rajdeep.sharma17@gmail.com>
2018-02-02 12:10:54 +0530

    Added Cards

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
     return(
-        <div className = 'bg-ight-green dib br3 pr3'>
+        <div className = 'bg-ight-green dib br3 pr3 ma2 grow'>
             <img src="https://robohash.org/batman?200x200" alt="roboots"/>
             <div>
                 <h2>Jane Doe</h2>
