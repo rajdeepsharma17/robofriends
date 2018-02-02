55020b6d51c3d71ca39e2edaa729ceda4028db65
rajdeepsharma17 <rajdeep.sharma17@gmail.com>
2018-02-02 14:20:22 +0530

    Added CardList

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({id, name, email}) => {
     return(
         <div className = 'tc bg-light-green dib br3 pr3 ma2 grow bw2 shadow-5'>
-            <img src="https://robohash.org/{id}?200x200" alt="roboots"/>
+            <img src="https://robohash.org/${id}?200x200" alt="roboots"/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card'
+import CardList from './CardList.js'
 import {robots} from './robots.js'
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-ReactDOM.render(<div>
+ReactDOM.render(<div>git
                     <Card id = {robots[0].id} name = {robots[0].name} email = {robots[0].email} />
                     <Card id = {robots[1].id} name = {robots[1].name} email = {robots[1].email} />
                     <Card id = {robots[2].id} name = {robots[2].name} email = {robots[2].email} />
