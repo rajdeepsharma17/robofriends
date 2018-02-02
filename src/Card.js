1565b14db841bfa781c922f992612ba09f5615b2
rajdeepsharma17 <rajdeep.sharma17@gmail.com>
2018-02-02 12:07:41 +0530

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+        <div className = 'bg-ight-green dib br3 pr3'>
+            <img src="https://robohash.org/batman?200x200" alt="roboots"/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>JaneDoe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
