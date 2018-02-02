2688ba36bec22057a36e3834f647a85483f24e22
rajdeepsharma17 <rajdeep.sharma17@gmail.com>
2018-02-02 23:41:41 +0530

    enhanced Search

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({searchChange, searchField}) => {
     return(
         <div className = 'pa2'>
             <input 
             className = 'pa3 ba b--green bg-lightest-blue'
-            type="search" placeholder = "Search Robots"/>    
+            type="search" placeholder = "Search Robots"
+            onChange = {searchChange} />    
         </div>
         
     )
