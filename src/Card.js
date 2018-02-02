3bc70af1946b05df631671518507023c7ff0d610
rajdeepsharma17 <rajdeep.sharma17@gmail.com>
2018-02-02 12:53:57 +0530

    git

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({id, name, email}) => {
     return(
-        <div className = 'bg-ight-green dib br3 pr3 ma2 grow'>
-            <img src="https://robohash.org/batman?200x200" alt="roboots"/>
+        <div className = 'tc bg-light-green dib br3 pr3 ma2 grow bw2 shadow-5'>
+            <img src="https://robohash.org/{id}?200x200" alt="roboots"/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>JaneDoe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
